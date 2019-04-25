09c95c25e0ccdd3571acc0b5eb374bff94cf33ee
zdd <635518693@qq.com>
2019-04-26 00:29:45 +0800

    home201904250000

--- nsd1811/devops/day04/httperror.py
+++ nsd1811/devops/day04/httperror.py
@@ -5,7 +5,7 @@ url2 = 'http://127.0.0.1/ban'
 try:
     r1 = request.urlopen(url1)
 except error.HTTPError as e:  # 把异常捕获后起名为e，e只是个变量
-    print('错误:', e)
+    print('错误:', e)         #输出了捕获的报错信息，程序不会终止
 
 try:
     r2 = request.urlopen(url2)
